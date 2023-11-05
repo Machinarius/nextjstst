2b081d70905035a07079fa8feec26a11825dd6aa
Germán Valencia <machinarius92@gmail.com>
2023-11-04 20:27:28 -0500

    Enabled Server-Side data fetching

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,37 @@
 import React from "react";
+import { lusitana } from "@ui/fonts";
+import RevenueChart from "@ui/dashboard/revenue-chart";
+import LatestInvoices from "@ui/dashboard/latest-invoices";
+import { fetchCardData } from "@/app/lib/data";
+import { Card } from "@ui/dashboard/cards";
 
 // The page.tsx file is automatically rendered by NextJS as the content of the route
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const {
+    totalPaidInvoices,
+    totalPendingInvoices,
+    numberOfInvoices,
+    numberOfCustomers
+  } = await fetchCardData();
   return (
-    <p>Dashboard</p>
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart />
+        <LatestInvoices />
+      </div>
+    </main>
   );
 }
